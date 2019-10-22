b44f9a45a7647e095bc97254593b78ae79410c04
53942392 <53942392+Cai-Edwards@users.noreply.github.com>
2019-10-22 13:32:33 +0100

    Added get_ids and get_appointments

--- scripts/database.py
+++ scripts/database.py
@@ -13,14 +13,51 @@ def connection(dataBase): #Connect to the database
 def dataToPe(db, yeargroup):
     q = "insert into pe.relationships select p.pid, t.tid, t.classID, NULL from data.classteacher t inner join data.classpupil p on t.classID=p.classID where t.yeargroup=%s;"
     cursor = db.cursor()
-    
     cursor.execute(q, (yeargroup, ))
-    print("Executed")
 
     db.commit()
 
-db = connection("pe")
+    return "Complete"
 
-dataToPe(db, 12)
+def clear_pe(db):
+    cursor = db.cursor()
+    q1 = "truncate table relationships"
+    cursor.execute(q1)
 
+    db.commit()
+
+    return "Complete"
+
+def get_appointments(db, group):
+    cursor = db.cursor()
+
+    q1 = "SELECT {}, group_concat({}) from relationships group by {}"
+
+    if group[0].lower() == "p":
+        cursor.execute(q1.format("pid", "tid", "pid"))
+    elif group[0].lower() == "t":
+        cursor.execute(q1.format("tid", "pid", "tid"))
+    else:
+        return "Not a valid input"
+    
+    appointments = {}
+
+    for i in cursor.fetchall():
+        appointments[int(i[0])] = list(map(int, i[1].split(",")))
+
+    return appointments
+
+def get_ids(db, group):
+    cursor = db.cursor()
+
+    q1 = "SELECT {} from relationships;"
+
+    if group[0].lower() == "p":
+        cursor.execute(q1.format("pid"))
+    elif group[0].lower() == "t":
+        cursor.execute(q1.format("tid"))
+    else:
+        return "Not a valid input"
+
+    return [x[0] for x in cursor.fetchall()]
 
