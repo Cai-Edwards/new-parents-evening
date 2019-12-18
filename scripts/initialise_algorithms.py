23173e3430ea4ec5ef976880fbcf2aee25dcfc90
53942392 <53942392+Cai-Edwards@users.noreply.github.com>
2019-12-18 10:57:40 +0000

    Added shake, variable and ordered algorithms

--- scripts/initialise_algorithms.py
+++ scripts/initialise_algorithms.py
@@ -3,6 +3,7 @@
 from database import *
 from output import *
 from list_manipulation import *
+from random import randint
 
 def first_fit(db, group, appointments, init=50, increase = 5):
     '''First fit algorithm. The opposite appointment data needs to be provided. Can be done to teachers or pupils'''
@@ -113,4 +114,131 @@ def first_few(db, group, appointments, init=30, increment=5, increase=3):
 
     return remove_excess(timetable)
 
+def shake_first_fit(db, group, appointments, init=50, increase = 5):
+    '''First fit but shaking'''
+
+    timetable = {}
+
+    for person in get_ids(db, group[0].lower()):
+        timetable[person] = [0 for x in range(init)] #Fills their ids with 0's
+
+    num = 0
+    for person in appointments:
+        slots_taken = []
+        num += 1
+
+        for relationships in appointments[person]:
+            if num % 2 == 0:
+                slot = 0
+
+                while True:
+                    if slot >= len(timetable[relationships]):
+                        timetable[relationships].extend([0,]*increase)
+                    
+                    if timetable[relationships][slot] == 0 and slot not in slots_taken:
+                        timetable[relationships][slot] = person
+                        slots_taken.append(slot)
+                        break
+
+                    else:
+                        slot += 1
+
+            else:
+                slot = len(timetable[relationships])-1
+                while True:
+                    
+                    if slot < 0:
+                        timetable[relationships].extend([0,]*increase)
+                        slot = len(timetable[relationships])-1
+                    
+                    if timetable[relationships][slot] == 0 and slot not in slots_taken:
+                        timetable[relationships][slot] = person
+                        slots_taken.append(slot)
+                        break
+
+                    else:
+                        slot -= 1
+
+    return remove_excess(timetable)
+
+def ordered(db, group, appointments, init=50, increase = 5, longest=True):
+    '''Order by longest/shortest pupil and teacher'''  
+
+    longest_person = {x:appointments[x] for x in sorted(appointments, key=lambda x: len(appointments[x]), reverse=True)}
+
+    cursor = db.cursor()
+
+    q1 = "SELECT {}, count({}) from relationships group by {} order by COUNT({}) DESC" 
+
+    if group[0].lower() == "p":
+        cursor.execute(q1.format("pid", "tid", "pid", "tid"))
+    elif group[0].lower() == "t":
+        cursor.execute(q1.format("tid", "pid", "tid", "pid"))
+    else:
+        return "Not a valid input"
     
+    lengths = dict(cursor.fetchall())
+
+    for person in longest_person:
+        values = [(ids, lengths[ids]) for ids in longest_person[person]]
+        longest_person[person] = [x[0] for x in sorted(values, key=lambda k: k[1], reverse=True)]
+
+    timetable = {}
+
+    for person in get_ids(db, group[0].lower()):
+        timetable[person] = [0 for x in range(init)] #Fills their ids with 0's
+
+    for person in longest_person:
+        slots_taken = []
+
+        for relationships in longest_person[person]:
+            slot = 0
+
+            while True:
+                if len(timetable[relationships]) <= slot:
+                    timetable[relationships].extend([0,] * increase)
+
+                if timetable[relationships][slot] == 0 and slot not in slots_taken:
+                    timetable[relationships][slot] = person
+                    slots_taken.append(slot)
+                    break
+
+                else:
+                    slot += 1    
+
+    return remove_excess(timetable)
+
+def variable_first_fit(db, group, appointments, init=50, increase = 5):
+    '''First fit starting at random positions'''
+    
+    timetable = {}
+
+    for person in get_ids(db, group[0].lower()):
+        timetable[person] = [0 for x in range(init)] #Fills their ids with 0's
+
+    for person in appointments:
+        slots_taken = []
+
+        init = randint(0, len(appointments[person]))
+
+        for relationships in appointments[person]:
+            slot = init
+
+            if slot >= len(timetable[relationships]):
+                slot = len(timetable[relationships])
+                init = slot
+
+            while True:
+
+                if len(timetable[relationships]) <= slot:
+                    timetable[relationships].extend([0,] * increase)
+
+                if timetable[relationships][slot] == 0 and slot not in slots_taken:
+                    timetable[relationships][slot] = person
+                    slots_taken.append(slot)
+                    break
+
+                else:
+                    slot += 1
+
+    return remove_excess(timetable)
