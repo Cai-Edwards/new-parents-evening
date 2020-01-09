28054b22306f447d22224c3d91944df4b23308d2
53942392 <53942392+Cai-Edwards@users.noreply.github.com>
2020-01-09 21:30:02 +0000

    Started simulated annealing

---
+++ scripts/adaptive_algorithms.py
@@ -0,0 +1,32 @@
+from analysis import analysis
+from dict_manipulation import *
+from random import random
+import numpy as np
+
+def anneal(timetable, max_step):
+    '''Simulated annealing'''
+
+    old_score = analysis(timetable)['score']
+
+    for i in range(1, max_step):
+        T = i/max_step
+
+        new = shift_left(swap_existing_columns(timetable))
+
+
+        new_score = analysis(new)['score']
+
+        if new_score < old_score:
+            timetable = new
+            old_score = new_score
+        elif np.exp(-(new_score-old_score)/T) > random():
+            timetable = new
+            old_score = new_score
+
+        print(new_score, old_score)
+
+    return timetable
+
+    
+
+
