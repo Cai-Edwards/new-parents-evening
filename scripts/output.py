99b4628ac5011c949b7c6b5b2552d8520b6ec297
53942392 <53942392+Cai-Edwards@users.noreply.github.com>
2020-01-29 20:24:14 +0000

    added draw timetable

--- scripts/output.py
+++ scripts/output.py
@@ -2,6 +2,9 @@ import csv
 import matplotlib.pyplot as plt
 from list_manipulation import dictToList
 from scipy.stats import linregress #INPLEMENT BY SELF
+from dict_manipulation import longest
+import pygame
+from random import randint
 
 def write(timetable, file):
     '''Write a timetable to a csv file
@@ -58,5 +61,53 @@ def visualise(analysis):
 
             ax[0].plot([0, max(x)], [c, b*max(x)+c])
 
-    
     plt.show()
+
+def draw_timetable(timetable):
+    
+    rows = longest(timetable) + 1
+    columns = len(timetable) + 1
+    
+    pygame.init()
+
+    d = pygame.display.set_mode((600, 300), pygame.RESIZABLE)
+    pygame.display.set_caption("Timetable")
+
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    green = (0, 125, 0)
+
+    offset = 5
+
+    while True:
+        d.fill(black)
+
+        w = d.get_width()
+        h = d.get_height()
+
+        cx = round(w/columns)
+        cy = round(h/rows)
+
+        for i, person in enumerate(timetable):
+            for k, slot in enumerate(timetable[person]):
+                    
+                if k == 0 or i == 0:
+                    pass
+                elif slot != 0:
+                    pygame.draw.rect(d, green, (cx*(k), cy*(i), cx*(k+1), cy*(i+1)))
+
+        for k in range(rows):
+            pygame.draw.line(d, white, (cx*(k+1), 0), (cx*(k+1), h))
+        for i in range(columns):
+            pygame.draw.line(d, white, (0, cy*(i+1)), (w, cy*(i+1)))
+        
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+
+            if event.type == pygame.VIDEORESIZE:
+                d = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
